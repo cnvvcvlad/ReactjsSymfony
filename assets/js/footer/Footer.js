25c3595de3e7c51b64e01f4214443f40ee422756
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-07-24 10:18:33 +0200

    completed footer with another links

--- assets/js/footer/Footer.js
+++ assets/js/footer/Footer.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            Footer
+            <ul>
+                <li>Adresse</li>
+                <li>Reseaux sociaux</li>
+                <li>FAQ</li>
+                <li>Mentions légales</li>
+            </ul>
+            <div>© Copyright 2020</div>
         </div>
     );
 };
