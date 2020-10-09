8b7885634f4467e30924b9410c526a476660369e
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-10-10 01:17:14 +0200

    changed acces control

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,12 +14,11 @@ class ArticleController extends AbstractController
      * @Route("/api/articles", name="articles")
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function getArticles(Request $request)
+    public function getArticles()
     {
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
-
+        
         //serialisation(normalisation + encodage)
-        return $this->json($articles, 200, ['Access-Control-Allow-Origin'=>'https://127.0.0.1:8000
-'], ['groups' => 'post:read']);
+        return $this->json($articles, 200, ['Access-Control-Allow-Origin : "Content-Type", "Authorization"'], ['groups' => 'post:read']);
     }
 }
